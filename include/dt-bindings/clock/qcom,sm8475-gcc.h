94ad697c527862a4dc054a2d1b09cdf78e49a589
Danila Tikhonov <danila@jiaxyga.com>
2024-04-03 23:50:27 +0300

    dt-bindings: clock: qcom,gcc-sm8450: Add SM8475 GCC bindings
    Add new entry to the SM8450 dt-bindings and create a new header
    file for the SM8475 clocks.
    
    Signed-off-by: Danila Tikhonov <danila@jiaxyga.com>

---
+++ include/dt-bindings/clock/qcom,sm8475-gcc.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2024, Danila Tikhonov <danila@jiaxyga.com>
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_GCC_SM8475_H
+#define _DT_BINDINGS_CLK_QCOM_GCC_SM8475_H
+
+#include "qcom,gcc-sm8450.h"
+
+/* GCC clocks */
+#define GCC_GPLL2						185
+#define GCC_GPLL3						186
+
+#endif
